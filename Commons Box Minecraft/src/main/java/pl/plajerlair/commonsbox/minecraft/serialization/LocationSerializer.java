5db9e10bfd230e4b8235ae06f7dc0a42c0cbd4c6
Tigerpanzer_02 <legionofunicorns0202@gmail.com>
2021-03-14 13:49:55 +0100

    Create world on getLocation (no need for mv)

--- Commons Box Minecraft/src/main/java/pl/plajerlair/commonsbox/minecraft/serialization/LocationSerializer.java
+++ Commons Box Minecraft/src/main/java/pl/plajerlair/commonsbox/minecraft/serialization/LocationSerializer.java
@@ -3,6 +3,7 @@ package pl.plajerlair.commonsbox.minecraft.serialization;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.WorldCreator;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -53,6 +54,9 @@ public class LocationSerializer {
     }
     String[] loc = path.split(",");
     World world = Bukkit.getServer().getWorld(loc[0]);
+    if(world == null) {
+      world = Bukkit.createWorld(new WorldCreator(loc[0]));
+    }
     double x = Double.parseDouble(loc[1]);
     double y = Double.parseDouble(loc[2]);
     double z = Double.parseDouble(loc[3]);
